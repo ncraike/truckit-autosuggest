7307bed273c9dd6b6f6545dc80274e886d68cad1
Natasha C <natasha-git.film372@passinbox.com>
2025-02-09 00:46:53 +1000

    Improve autosuggest endpoint
    Improve autosuggest endpoint's handling of error cases:
    - no "query" argument given (gives HTTP 400)
    - category of item is unknown (gives HTTP 404)
    
    Also fixes Bottle's error handling to give required JSON responses.
    
    Adds docstrings.

--- src/autosuggest/app.py
+++ src/autosuggest/app.py
@@ -1,31 +1,45 @@
+import json
+
 from bottle import Bottle, abort, request
 
 from autosuggest.categories import CATEGORIES, build_mapping, lookup_item
 
-app = Bottle()
-app.config["categories"] = build_mapping(CATEGORIES)
+
+class BottleWithJSONErrors(Bottle):
+    """Override Bottle's default error handling to give JSON-formatted errors."""
+
+    def default_error_handler(self, res):
+        res.content_type = "application/json"
+        return json.dumps({"message": res.body})
+
+
+app = BottleWithJSONErrors()
+app.config["categories_mapping"] = build_mapping(CATEGORIES)
+
 
 @app.route("/autosuggest")
-def autosuggest():
+def autosuggest() -> dict[str, str]:
+    """Handle requests for /autosuggest?query=<item> .
+
+    Will describe the category of the queried item in a JSON response.
+
+    For example,
+
+    GET /autosuggest?query=banana
+    -> {"message": "Banana is a fruit"}
+    """
     try:
         query = request.query["query"]
     except KeyError:
-        abort(400, "Please include a 'query' argument")
+        abort(400, "Please include a 'query' argument in the query string")
 
-    category = lookup_item(app.config["categories"], query)
+    mapping = app.config["categories_mapping"]
+    category = lookup_item(mapping, query)
     if category is None:
-        abort(404, f"No catgegory found for '{query}'")
+        abort(404, f"No category found for '{query}'")
 
-    return {
-        "message": f"{query.title()} is a {category}"
-    }
-
-
-@app.error(400)
-@app.error(404)
-def handle_error(error):
-    return {"message": error}
+    return {"message": f"{query.title()} is a {category}"}
 
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=8080, debug=True)
+    app.run(host="localhost", port=8080, debug=True)
