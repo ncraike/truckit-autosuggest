f378ee57a7d69dc17b8cfe077cc68b75cf37798f
Natasha C <natasha-git.film372@passinbox.com>
2025-02-10 14:47:21 +1000

    Add debounce on text input so we don't spam API
    Use the popular "debounce" package to delay querying the backend until
    the user stops typing for 200ms.
    
    This avoid an API request for every letter typed (after the second
    letter) as the user is typing the item name, e.g. typing "banana" sends
    4 API requests instead of just 1.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 // import './style.css'
 
-import { lookupCategory } from "./autosuggest";
+import debounce from 'debounce';
 
-const MIN_QUERY_LEN = 3;
+import { lookupCategory } from './autosuggest';
+
+const QUERY_MIN_LENGTH = 3;
+const QUERY_DEBOUNCE_MS = 200;
+
 
 export function setupAutosuggest(queryField: HTMLInputElement) {
-    queryField.addEventListener('input', (_event) => {
-        updateAutosuggest(queryField);
-    });
+    const debouncedUpdate = debounce(
+        (_event) => {
+            updateAutosuggest(queryField);
+        },
+        QUERY_DEBOUNCE_MS
+    );
+
+    queryField.addEventListener('input', debouncedUpdate);
 }
 
 export async function updateAutosuggest(queryField: HTMLInputElement) {
     const query = queryField.value
     const responseElement = document.querySelector('#response')!;
-    if (query && query.length >= MIN_QUERY_LEN) {
+    if (query && query.length >= QUERY_MIN_LENGTH) {
         const response = await lookupCategory(query.toString());
         if (response) {
             responseElement.innerHTML = response;
